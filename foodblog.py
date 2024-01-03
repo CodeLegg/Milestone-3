7cad8dde7dcaab492ea78d9567dc9468d34dd77e
CodeLegg <michaelraymondlegg@gmail.com>
2024-01-03 23:20:38 +0000

    Add registration and login routes and forms

--- foodblog.py
+++ foodblog.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '4567bfpahg309bndh357hfpsba245'
 
 posts = [
     {
@@ -17,13 +20,23 @@ posts = [
 
 ]
 
-@app.route("/")
+@app.route('/')
 def home():
     return render_template('home.html', posts = posts)
 
-@app.route("/")
+@app.route('/')
 def about():
-    return render_template('about.html')
+    return render_template('about.html', title='')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title='Register', form=form)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
 
 @app.route("/")
 def layout():
