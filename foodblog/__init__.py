0c326b31007612fd75ce729e7f410bfbabfeaca9
CodeLegg <michaelraymondlegg@gmail.com>
2024-01-05 03:47:22 +0000

    Add Flask extensions for authentication and security
    This commit adds the Flask extensions Flask-Bcrypt, Flask-Login, and Flask-WTF-CSRFProtect to the project. These extensions provide functionality for password hashing, user authentication, and CSRF protection.

--- foodblog/__init__.py
+++ foodblog/__init__.py
@@ -1,13 +1,21 @@
-
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
 from flask_wtf.csrf import CSRFProtect
+
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '4567bfpahg309bndh357hfpsba245'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'dark'
 csrf = CSRFProtect(app)
 
 from foodblog import routes
+
