42eb720d9b1057bcbb253a53ddd591a3b8288864
CodeLegg <michaelraymondlegg@gmail.com>
2024-01-05 03:47:48 +0000

    Add login and logout functionality

--- foodblog/routes.py
+++ foodblog/routes.py
@@ -1,7 +1,8 @@
-from flask import render_template, url_for, request, flash, redirect
-from foodblog import app
+from flask import render_template, url_for, request, flash, redirect, request
+from foodblog import app, db, bcrypt
 from foodblog.forms import RegistrationForm, LoginForm
 from foodblog.models import User, Post, Comment
+from flask_login import login_user, current_user, logout_user, login_required
 
 posts = [
     {
@@ -18,53 +19,81 @@ posts = [
     }
 
 ]
+
 # HOME PAGE
-@app.route('/home')
+@app.route('/')
 def home():
     return render_template('home.html', posts = posts)
 
 # MEALS PAGE
 @app.route('/meals')
+@login_required
 def meals():
-    return render_template('meals.html', title='BiteBurst - Meals')
+    return render_template('meals.html', title='Meals')
 
 # RECEIPES PAGE
 @app.route('/receipes')
+@login_required
 def receipes():
-    return render_template('receipes.html', title='BiteBurst - Receipes')
+    return render_template('receipes.html', title='Receipes')
 
 # COOKING TIPS PAGE
 @app.route('/cooking_tips')
+@login_required
 def cooking_tips():
-    return render_template('cooking_tips.html', title='BiteBurst - Cooking Tips')
+    return render_template('cooking_tips.html', title='Cooking Tips')
 
 # DISCUSSION PAGE
 @app.route('/discussion')
+@login_required
 def discussion():
-    return render_template('discussion.html', title='BiteBurst - Discussion')
+    return render_template('discussion.html', title='Discussion')
 
 # SIGN UP/REGISTER PAGE
 @app.route('/register', methods=['GET', 'POST'])
-def register():
+def register(): 
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = RegistrationForm()
 
     if request.method == 'POST' and form.validate_on_submit():
-        flash(f'Account created for {form.username.data}!', 'success')
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password, favorite_food=form.favorite_food.data )
+        db.session.add(user)
+        db.session.commit()
+        flash(f'Your account has been created! You are now able to login', 'success')
         return redirect(url_for('login'))
-    return render_template('register.html', form=form)
+    return render_template('register.html', title='Sign Up', form=form)
 
 # LOGIN PAGE
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = LoginForm()
     
     if form.validate_on_submit():
-        if form.username.data == 'CodeLegg' and form.email.data == 'CodeLegg@gmail.com' and form.password.data == 'Password12!':
-            flash(f"You've Succsessfly Logged In {form.username.data}!", 'success')
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            next_page = request.args.get('next')
             return redirect(url_for('home'))
         else:
-            flash(f'Login Unsuccessful. Please check username and password', 'danger')
+            flash(f'Login Unsuccessful. Please check Username Email and Password', 'danger')
     return render_template('login.html', title='Login', form=form)
+
+# LOGOUT PAGE
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+   
+
+# LAYOUT/SITE NAVBAR & CONTENT
+@app.route("/profile")
+@login_required
+def profile():
+    return render_template('profile.html', title='Profile')
 
 # LAYOUT/SITE NAVBAR & CONTENT
 @app.route("/layout")
