d76d76ea3e2d2d363f1f8acb5a3ebba9768adef9
CodeLegg <michaelraymondlegg@gmail.com>
2024-01-17 20:41:33 +0000

    Remove unnecessary code and comments

--- foodblog/__init__.py
+++ foodblog/__init__.py
@@ -3,8 +3,6 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 import os
-# from flask_wtf.csrf import CSRFProtect
-
 
 
 app = Flask(__name__)
@@ -16,16 +14,12 @@ if uri.startswith("postgres://"):
     uri = uri.replace("postgres://", "postgresql://", 1)
 app.config['SQLALCHEMY_DATABASE_URI'] = uri
 
-# app.config['SECRET_KEY'] = '4567bfpahg309bndh357hfpsba245'
-# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@localhost:5432/foodblog'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'dark'
-# csrf = CSRFProtect(app)
 
 from foodblog import routes
 
