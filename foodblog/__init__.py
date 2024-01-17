26c0de61050673f68be6502f7b09edc68adc1b90
CodeLegg <michaelraymondlegg@gmail.com>
2024-01-17 18:41:18 +0000

    Update SECRET_KEY and SQLALCHEMY_DATABASE_URI

--- foodblog/__init__.py
+++ foodblog/__init__.py
@@ -2,14 +2,24 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
-
+import os
 # from flask_wtf.csrf import CSRFProtect
 
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '4567bfpahg309bndh357hfpsba245'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@localhost:5432/foodblog'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY',
+                                            'default_fallback_key')
+
+uri = os.environ['DATABASE_URL']
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
+app.config['SQLALCHEMY_DATABASE_URI'] = uri
+
+# app.config['SECRET_KEY'] = '4567bfpahg309bndh357hfpsba245'
+# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@localhost:5432/foodblog'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
