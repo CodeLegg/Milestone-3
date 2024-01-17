fa5a2d4fd91f9fa3e9699e6cbbd68b919d7f8ee6
CodeLegg <michaelraymondlegg@gmail.com>
2024-01-17 18:22:26 +0000

    Update database URI in config

--- foodblog/__init__.py
+++ foodblog/__init__.py
@@ -10,9 +10,9 @@ from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '4567bfpahg309bndh357hfpsba245'
-# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL' , 'postgres://hkuzcfrwsqfxef:8e058f0d64be67a32104dc79dd25cd116c882c0beb3a2715bbc693992547f7f2@ec2-54-73-22-169.eu-west-1.compute.amazonaws.com:5432/dv09pn3hrk504')
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@localhost:5432/foodblog'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@localhost:5432/foodblog'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
