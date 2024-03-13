4f36cd22ad9bdfda839745c9952998fec156aa97
CodeLegg <michaelraymondlegg@gmail.com>
2024-03-13 19:28:20 +0000

    Remove commented out code for password reset functionality

--- foodblog/routes.py
+++ foodblog/routes.py
@@ -197,45 +197,6 @@ def delete_post(post_id):
     flash('Your post has been deleted!', 'success')
     return redirect(url_for('discussion'))
 
-# def send_reset_email(self):
-#         token = self.get_reset_token()
-#         msg = Message('Password Reset Request', sender='noreply@demo.com', recipients=[self.email])
-#         msg.body = f'''To reset your password, visit the following link:
-# {url_for('reset_token', token=token, _external=True)}
-
-# If you did not intend to reset your password, simply ignore this email and no changes will be made.
-# '''
-#         mail.send(msg)
-
-# @app.route("/reset_password", methods=['GET', 'POST'])
-# def reset_request():
-#     if current_user.is_authenticated:
-#         return redirect(url_for('home'))
-#     form = RequestResetForm()
-#     if form.validate_on_submit():
-#         user = User.query.filter_by(email=form.email.data).first()
-#         send_reset_email(user)
-#         flash('An email has been sent with instructions to reset your password.', 'success')
-#         return redirect(url_for('login'))
-#     return render_template('reset_request.html', title='Reset Password', form=form)
-
-# @app.route("/reset_password/<token>", methods=['GET', 'POST'])
-# def reset_token(token):
-#     if current_user.is_authenticated:
-#         return redirect(url_for('home'))
-#     user = User.verify_reset_token(token)
-#     if user is None:
-#         flash('That is an invalid or expired token', 'warning')
-#         return redirect(url_for('reset_request'))
-#     form = ResetPasswordForm()
-#     if request.method == 'POST' and form.validate_on_submit():
-#         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-#         user.password = hashed_password
-#         db.session.commit()
-#         flash(f'Your password has been UPDATED! You are now able to login', 'success')
-#         return redirect(url_for('login'))
-#     return render_template('reset_token.html', title='Reset Password', form=form)
-
 
 
 # RECEIPE PAGES
