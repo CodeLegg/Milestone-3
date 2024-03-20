41c42adc15e6b69bc9a0276170d11f8ba4f91b17
CodeLegg <michaelraymondlegg@gmail.com>
2024-03-20 14:17:56 +0000

    Add dotenv package to load environment variables

--- foodblog/__init__.py
+++ foodblog/__init__.py
@@ -2,8 +2,12 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from dotenv import load_dotenv
+
 import os
 
+# Load environment variables from .env file
+load_dotenv()
 
 app = Flask(__name__)
 # app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY", "default_fallback_key")
