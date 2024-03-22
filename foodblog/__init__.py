6cbbc0417da17ef5b4cbdda86583338c5eeac0ec
CodeLegg <michaelraymondlegg@gmail.com>
2024-03-22 10:16:43 +0000

    Update database configuration

--- foodblog/__init__.py
+++ foodblog/__init__.py
@@ -3,32 +3,22 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 import os
+from dotenv import load_dotenv
 
+# Load environment variables from .env file
+load_dotenv()
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY", "default_fallback_key")
+# app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY", "default_fallback_key")
 
-uri = os.environ['DATABASE_URL']
-if uri.startswith("postgres://"):
-    uri = uri.replace("postgres://", "postgresql://", 1)
-app.config['SQLALCHEMY_DATABASE_URI'] = uri
-
-# Check and modify the URI if necessary
-if uri.startswith("postgres://"):
-    uri = uri.replace("postgres://", "postgresql://", 1)
-
-# Replace 'your_username', 'your_password', and 'mydatabase'
-# with your actual PostgreSQL
-# credentials and database name
-
-# local_db_url = f"postgresql://postgres:password@localhost:5432/foodblog"
-
-# # Update the database URL based on the environment
-# uri = os.environ.get("DATABASE_URL", local_db_url)
-
-# # Set the Flask app configuration
+# uri = os.environ["DATABASE_URL"]
+# if uri.startswith("postgres://"):
+#     uri = uri.replace("postgres://", "postgresql://", 1)
 # app.config["SQLALCHEMY_DATABASE_URI"] = uri
 
+# # Check and modify the URI if necessary
+# if uri.startswith("postgres://"):
+#     uri = uri.replace("postgres://", "postgresql://", 1)
 
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
